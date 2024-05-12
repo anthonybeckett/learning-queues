ad4d722e6087adf89f806fcaef3ee66a112467f7
AnthonyBeckett <antneyb10@gmail.com>
2024-05-12 09:00:38 +0100

    Added example on how laravels pipeline works

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,21 @@ Route::get('/', function (): string {
 
     return "Finished";
 });
+
+Route::get('/pipeline-example', function (): string {
+    $pipeline = app(\Illuminate\Pipeline\Pipeline::class);
+
+    $pipeline->send('hello world')
+        ->through([
+            function (string $string, Closure $next) {
+                $string = ucwords($string);
+
+                return $next($string);
+            }
+        ])
+        ->then(function (string $string) {
+            dump($string);
+        });
+
+    return 'Done';
+});
